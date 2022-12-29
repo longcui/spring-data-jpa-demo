e7e12f2f916ed8354083a6d7ab15bfd0e15ef014
Long Cui <long.cui@kongsbergdigital.com>
2022-12-29 16:24:15 +0800

    first commit

---
+++ src/main/java/com/example/spring/jpa/demo/Book.java
@@ -0,0 +1,24 @@
+package com.example.spring.jpa.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    //getters, setters, toString...
+}
